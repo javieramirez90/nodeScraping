b1d497db51ffd97f1e99dba2d9fc8a2ba6a6c8bc
xavier <javier.ramirez@aiesec.net>
2019-06-09 23:51:29 -0500

    Multiple elements selection
    Using a callback to use each element selected.

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const fs = require("fs");
 const cheerio = require("cheerio");
 
 async function main() {
-  const html = await request.get("https://reactnativetutorial.net/css-selectors/");
+  const html = await request.get("https://reactnativetutorial.net/css-selectors/lesson2.html");
   fs.writeFileSync("./test.html", html);
   const $ = await cheerio.load(html);
-  const theText = $("h1").text()
-
-  console.log(theText);
+  
+  $("h2").each((index, element) => {
+    console.log($(element).text());
+  });
 }
 
 main();
