22e0cc9f91f404682255c63fcb81319496dbd5f2
xavier <javier.ramirez@aiesec.net>
2019-06-10 00:27:01 -0500

    Attribute selector

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const fs = require("fs");
 const cheerio = require("cheerio");
 
 async function main() {
-  const html = await request.get("https://reactnativetutorial.net/css-selectors/lesson2.html");
+  const html = await request.get("https://reactnativetutorial.net/css-selectors/lesson6.html");
   fs.writeFileSync("./test.html", html);
   const $ = await cheerio.load(html);
+
+  //attribute selector
+  const attr = $('[data-customer="22293"]')
   
   $("h2").each((index, element) => {
     console.log($(element).text());
   });
+
+  console.log(attr.text());
 }
 
 main();
